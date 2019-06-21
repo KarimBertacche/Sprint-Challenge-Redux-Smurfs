88d5b8d39021977d0584ca8281b55f921e2ada64
Karim B <mattiakarim@live.it>
2019-06-21 09:52:34 +0200

    Started with adding fetch method to action and reducers, completed index.js file

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,15 +1,19 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+import axios from 'axios';
 
-/*
-  For this project you'll need at least 2 action creators for the main portion,
-   and 2 more for the stretch problem.
-   Be sure to include action types for each type of action creator. Also, be sure to mind
-     the "pending" states like, fetching, creating, updating and deleting.
-   C - addSmurf
-   R - getSmurfs
-   U - updateSmurf
-   D - deleteSmurf
-*/
+export const FETCH_START = 'FETCH_START';
+export const FETCH_SUCCESS = 'FETCH_SUCCESS';
+export const FETCH_FAILURE = 'FETCH_FAILURE';
+
+const smurfsData = 'http://localhost:3333/smurfs';
+
+export const fetchingSmurfs = () => dispatch => {
+  dispatch({ type: FETCH_START })
+  axios
+    .get(smurfsData)
+    .then(response => {
+      dispatch({ type: FETCH_SUCCESS, payload: response.data });
+    })
+    .catch(error => {
+      dispatch({ type: FETCH_FAILURE, payload: error.message });
+    });
+}
